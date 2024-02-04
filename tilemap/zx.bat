218d46dc3bda476d235d2388a9ebad906de726a3
seedy1812 <63559352+seedy1812@users.noreply.github.com>
2024-02-04 22:15:59 +0000

    Delete tilemap/zx.bat

--- tilemap/zx.bat
+++
@@ -1,2 +0,0 @@
-cd C:\zx\ZEsarUX_win-7.1\
-zesarux.exe --noconfigfile --machine tbblue --realvideo --enabletimexvideo --tbblue-fast-boot-mode --sna-no-change-machine --enable-esxdos-handler --enable-mmc --mmc-file tbblue.mmc --enable-divmmc-ports --esxdos-root-dir "C:\zx\ZEsarUX_win-7.1" %1
